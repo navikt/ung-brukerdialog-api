34e550b1a727dd705df6808b33d43f49bdf50946
Espen Velsvik <espenjv@users.noreply.github.com>
2026-03-10 15:36:06 +0100

    Slett fjerne periode (#41)
    * Fjern oppgavetype for fjernet periode
    
    * Fiks orm

--- kontrakt/src/main/java/no/nav/ung/brukerdialog/kontrakt/oppgaver/OppgavetypeDataDto.java
+++ kontrakt/src/main/java/no/nav/ung/brukerdialog/kontrakt/oppgaver/OppgavetypeDataDto.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import no.nav.ung.brukerdialog.kontrakt.oppgaver.typer.endretperiode.EndretPeriodeDataDto;
 import no.nav.ung.brukerdialog.kontrakt.oppgaver.typer.endretsluttdato.EndretSluttdatoDataDto;
 import no.nav.ung.brukerdialog.kontrakt.oppgaver.typer.endretstartdato.EndretStartdatoDataDto;
-import no.nav.ung.brukerdialog.kontrakt.oppgaver.typer.fjernperiode.FjernetPeriodeDataDto;
 import no.nav.ung.brukerdialog.kontrakt.oppgaver.typer.inntektsrapportering.InntektsrapporteringOppgavetypeDataDto;
 import no.nav.ung.brukerdialog.kontrakt.oppgaver.typer.kontrollerregisterinntekt.KontrollerRegisterinntektOppgavetypeDataDto;
 import no.nav.ung.brukerdialog.kontrakt.oppgaver.typer.søkytelse.SøkYtelseOppgavetypeDataDto;
@@ -22,7 +21,6 @@ import no.nav.ung.brukerdialog.kontrakt.oppgaver.typer.søkytelse.SøkYtelseOppg
 @JsonSubTypes({
     @JsonSubTypes.Type(value = EndretStartdatoDataDto.class, name = "ENDRET_STARTDATO"),
     @JsonSubTypes.Type(value = EndretSluttdatoDataDto.class, name = "ENDRET_SLUTTDATO"),
-    @JsonSubTypes.Type(value = FjernetPeriodeDataDto.class, name = "FJERNET_PERIODE"),
     @JsonSubTypes.Type(value = EndretPeriodeDataDto.class, name = "ENDRET_PERIODE"),
     @JsonSubTypes.Type(value = KontrollerRegisterinntektOppgavetypeDataDto.class, name = "KONTROLLER_REGISTERINNTEKT"),
     @JsonSubTypes.Type(value = InntektsrapporteringOppgavetypeDataDto.class, name = "INNTEKTSRAPPORTERING"),
