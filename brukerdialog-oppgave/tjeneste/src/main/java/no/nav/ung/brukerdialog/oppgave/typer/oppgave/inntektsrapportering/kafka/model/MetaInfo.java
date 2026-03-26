53767ec35c0a477d269db95d769ad84cc734f764
Espen Velsvik <espen.velsvik@nav.no>
2026-03-26 14:30:25 +0100

    Fjerner asynkron håndtering av respons på oppgave

--- brukerdialog-oppgave/tjeneste/src/main/java/no/nav/ung/brukerdialog/oppgave/typer/oppgave/inntektsrapportering/kafka/model/MetaInfo.java
+++
@@ -1,9 +0,0 @@
-package no.nav.ung.brukerdialog.oppgave.typer.oppgave.inntektsrapportering.kafka.model;
-
-public record MetaInfo(
-    int version,
-    String correlationId,
-    String soknadDialogCommitSha
-) {
-}
-
--- brukerdialog-oppgave/tjeneste/src/main/java/no/nav/ung/brukerdialog/oppgave/typer/oppgave/søkytelse/kafka/model/MetaInfo.java
+++
@@ -1,9 +0,0 @@
-package no.nav.ung.brukerdialog.oppgave.typer.oppgave.søkytelse.kafka.model;
-
-public record MetaInfo(
-    int version,
-    String correlationId,
-    String soknadDialogCommitSha
-) {
-}
-
