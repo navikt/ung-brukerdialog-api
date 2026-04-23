9f56ef53f5f84f099b710de402b4e38b0b25d177
Espen Velsvik <espen.velsvik@nav.no>
2026-04-23 13:44:27 +0200

    Fjerner JsonFormat og JsonProperty

--- kontrakt/src/main/java/no/nav/ung/brukerdialog/kontrakt/oppgaver/typer/bosted/BekreftBostedOppgavetypeDataDto.java
+++ kontrakt/src/main/java/no/nav/ung/brukerdialog/kontrakt/oppgaver/typer/bosted/BekreftBostedOppgavetypeDataDto.java
@@ -1,8 +1,6 @@
 package no.nav.ung.brukerdialog.kontrakt.oppgaver.typer.bosted;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import no.nav.ung.brukerdialog.kontrakt.oppgaver.OppgaveType;
 import no.nav.ung.brukerdialog.kontrakt.oppgaver.OppgavetypeDataDto;
@@ -14,17 +12,12 @@ import java.time.LocalDate;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record BekreftBostedOppgavetypeDataDto(
-    @JsonProperty(value = "fom", required = true)
     @NotNull
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     LocalDate fom,
 
-    @JsonProperty(value = "tom", required = true)
     @NotNull
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     LocalDate tom,
 
-    @JsonProperty(value = "erBosattITrondheim", required = true)
     @NotNull
     Boolean erBosattITrondheim
 ) implements OppgavetypeDataDto {
