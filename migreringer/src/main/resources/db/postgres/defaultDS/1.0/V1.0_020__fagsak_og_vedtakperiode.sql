6dc04aec5eaa78316bc955fa943988cb6bcfbd56
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2026-09-05 12:33:25 +0200

    rydder opp skript - følger konvensjoner og fjerner overflødige kmmentarer
    Signed-off-by: Qadeer Khan <qadeer.ahmad.khan@nav.no>

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_020__fagsak_og_vedtakperiode.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_020__fagsak_og_vedtakperiode.sql
@@ -6,19 +6,18 @@ create table BD_FAGSAK
     id            bigint       not null primary key,
     aktoer_id     varchar(20)  not null,
     ytelse_type   varchar(50)  not null,
-    saksnummer    varchar(19)  not null,
+    saksnummer    varchar(19)  not null unique,
     versjon       bigint       not null default 0,
     opprettet_av  varchar(20)  not null default 'VL',
     opprettet_tid timestamp(3) not null default current_timestamp,
     endret_av     varchar(20),
-    endret_tid    timestamp(3),
-    constraint uq_BD_FAGSAK_saksnummer unique (saksnummer)
+    endret_tid    timestamp(3)
 );
 
-create index idx_BD_FAGSAK_aktoer_ytelse on BD_FAGSAK (aktoer_id, ytelse_type);
+create index idx_bd_fagsak_aktoer_ytelse on BD_FAGSAK (aktoer_id, ytelse_type);
 
-comment on table BD_FAGSAK is 'Speiling av saken i ung-sak, meldt inn ved iverksetting. Én rad per saksnummer, oppdatert i stedet for erstattet, slik at raden er en stabil identitet søknadshendelser kan peke på. At raden finnes betyr at ung-sak har fattet vedtak - en sak uten innvilgede perioder er noe annet enn en sak vi aldri har hørt om. Inneholder kun rådata; regelen for søknadsvindu utledes i TilgjengeligSøknadUtleder.';
-comment on column BD_FAGSAK.saksnummer is 'Saksnummeret i ung-sak. Unikt - en deltaker kan ha flere saker per ytelse.';
+comment on table BD_FAGSAK is 'Speiling av saken i ung-sak';
+comment on column BD_FAGSAK.saksnummer is 'Saksnummeret fra ung-sak';
 
 create table BD_VEDTAK_PERIODE
 (
@@ -33,15 +32,14 @@ create table BD_VEDTAK_PERIODE
     endret_tid    timestamp(3)
 );
 
-create index idx_BD_VEDTAK_PERIODE_fagsak on BD_VEDTAK_PERIODE (fagsak_id) where aktiv;
+create index idx_bd_vedtak_periode_fagsak on BD_VEDTAK_PERIODE (fagsak_id) where aktiv;
 
-comment on table BD_VEDTAK_PERIODE is 'Perioder ung-sak har vurdert vilkårene for. Skrives om i sin helhet ved hver melding: de forrige deaktiveres og beholdes, slik at det i ettertid går an å se hva saken så ut som da en søknad ble sluppet gjennom eller stoppet.';
-comment on column BD_VEDTAK_PERIODE.resultat is 'INNVILGET eller AVSLÅTT. Kun innvilgede perioder teller når søknadsvinduet utledes.';
-comment on column BD_VEDTAK_PERIODE.aktiv is 'False for perioder som er erstattet av en nyere melding fra ung-sak. Kun aktive perioder skal brukes til utledning.';
+comment on table BD_VEDTAK_PERIODE is 'Perioder med vedtak.';
+comment on column BD_VEDTAK_PERIODE.resultat is 'INNVILGET eller AVSLÅTT.';
 
 alter table BD_SOEKNAD_HENDELSE
     add column mottatt_i_fagsak bigint references BD_FAGSAK (id);
 
-create index idx_BD_SOEKNAD_HENDELSE_fagsak on BD_SOEKNAD_HENDELSE (mottatt_i_fagsak);
+create index idx_bd_soeknad_hendelse_fagsak on BD_SOEKNAD_HENDELSE (mottatt_i_fagsak);
 
-comment on column BD_SOEKNAD_HENDELSE.mottatt_i_fagsak is 'Fagsaken ung-sak meldte at søknaden er mottatt i. NULL betyr at søknaden fortsatt ligger til behandling og sperrer for ny søknad. Ortogonal til aktiv - en behandlet søknad er fortsatt aktiv.';
+comment on column BD_SOEKNAD_HENDELSE.mottatt_i_fagsak is 'Fagsaken ung-sak meldte at søknaden er mottatt i. NULL betyr at ung-sak har ikke behandlet saken.';
